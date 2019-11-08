9a0a65f352417c19700b5f6f9ba1a605b34fbbeb
sunchunfeng <516108736@qq.com>
2019-11-08 18:48:30 +0800

    1848

--- cluster/shard/api_backend.go
+++ cluster/shard/api_backend.go
@@ -273,6 +273,7 @@ func (s *ShardBackend) NewMinorBlock(block *types.MinorBlock,needBroadcast bool)
 func (s *ShardBackend) AddMinorBlock(block *types.MinorBlock) error {
 	fmt.Println("AddMinorBlock",block.Branch().Value,block.Hash().String())
 	if commitStatus := s.getBlockCommitStatusByHash(block.Hash()); commitStatus == BLOCK_COMMITTED {
+		fmt.Println("nnnnnnn")
 		return nil
 	}
 	//TODO support BLOCK_COMMITTING
@@ -280,9 +281,11 @@ func (s *ShardBackend) AddMinorBlock(block *types.MinorBlock) error {
 	_, xshardLst, err := s.MinorBlockChain.InsertChainForDeposits([]types.IBlock{block}, false)
 	if err != nil {
 		log.Error("Failed to add minor block", "err", err, "len", len(xshardLst))
+		fmt.Println("nnnnnnn-1")
 		return err
 	}
 	if len(xshardLst) != 1 {
+		fmt.Println("nnnnnnn-2")
 		log.Warn("Failed to add minor block-1", "len(XshardList)", len(xshardLst), "block.Number", block.Number(), "block.Hash", block.Hash().String())
 		return nil
 	}
@@ -294,22 +297,26 @@ func (s *ShardBackend) AddMinorBlock(block *types.MinorBlock) error {
 	if currHead.Hash() != s.MinorBlockChain.CurrentBlock().Hash() {
 		if err = s.broadcastNewTip(); err != nil {
 			s.setHead(currHead.Number)
+			fmt.Println("nnnnnnn-3")
 			return err
 		}
 	}
 
 	if xshardLst[0] == nil {
 		log.Info("add minor block has been added...", "branch", s.branch.Value, "height", block.Number())
+		fmt.Println("nnnnnnn-4")
 		return nil
 	}
 
 	prevRootHeight := s.MinorBlockChain.GetRootBlockByHash(block.PrevRootBlockHash()).Number()
 	if err := s.conn.BroadcastXshardTxList(block, xshardLst[0], prevRootHeight); err != nil {
 		s.setHead(currHead.Number)
+		fmt.Println("nnnnnnn-5")
 		return err
 	}
 	status, err := s.MinorBlockChain.GetShardStats()
 	if err != nil {
+		fmt.Println("nnnnnnn-6")
 		s.setHead(currHead.Number)
 		return err
 	}
