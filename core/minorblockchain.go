795940ed86bb8430dbe4f1d326dd899523c21882
sunchunfeng <516108736@qq.com>
2019-11-08 19:09:26 +0800

    1909

--- core/minorblockchain.go
+++ core/minorblockchain.go
@@ -1211,7 +1211,7 @@ func (m *MinorBlockChain) insertChain(chain []types.IBlock, verifySeals bool, is
 			block, err = it.next()
 		}
 		// Falls through to the block import
-
+		xShardList = append(xShardList, make([]*types.CrossShardTransactionDeposit,0))
 	// Some other error occurred, abort
 	case err != nil:
 		stats.ignored += len(it.chain)
